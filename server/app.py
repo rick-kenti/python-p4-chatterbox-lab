76a84306eef24e8d93564ede16cc84534e72bb84
rick-kenti <erickmakenti@gmail.com>
2026-02-21 23:01:35 +0300

    Create an API with Flask for a React frontend application.

--- server/app.py
+++ server/app.py
@@ -14,13 +14,61 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route("/messages", methods=["GET"])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    return make_response(
+        jsonify([message.to_dict() for message in messages]),
+        200
+    )
+
+@app.route("/messages", methods=["POST"])
+def create_message():
+    data = request.get_json()
+
+    new_message = Message(
+        body=data["body"],
+        username=data["username"]
+    )
+
+    db.session.add(new_message)
+    db.session.commit()
+
+    return make_response(
+        jsonify(new_message.to_dict()),
+        201
+    )
+
+@app.route("/messages/<int:id>", methods=["PATCH"])
+def update_message(id):
+    message = Message.query.get(id)
+
+    if not message:
+        return make_response({"error": "Message not found"}, 404)
+
+    data = request.get_json()
+
+    message.body = data.get("body", message.body)
+
+    db.session.commit()
+
+    return make_response(
+        jsonify(message.to_dict()),
+        200
+    )
+
+@app.route("/messages/<int:id>", methods=["DELETE"])
+def delete_message(id):
+    message = Message.query.get(id)
+
+    if not message:
+        return make_response({"error": "Message not found"}, 404)
+
+    db.session.delete(message)
+    db.session.commit()
+
+    return make_response({}, 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
